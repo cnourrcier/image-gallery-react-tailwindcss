c5796be429f93363a6b6a1e8daa9434ea99b82b0
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-05-20 15:33:29 -0500

    Clear search text after button press

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -4,9 +4,15 @@ const ImageCard = ({ image, searchTerm }) => {
 
     return (
         < div className="max-w-sm rounded overflow-hidden shadow-lg" >
-            <a href={image.webformatURL} target="_blank">
+            <a
+                href={image.webformatURL}
+                target="_blank"
+            >
                 {/* sets the width of an element to 100% of its parent container's width. */}
-                < img className="w-full duration-500 hover:scale-105 hover:shadow-xl" src={image.webformatURL} alt="" />
+                < img
+                    className="w-full duration-500 hover:scale-105 hover:shadow-xl"
+                    src={image.webformatURL} alt=""
+                />
             </a>
             <div className="px-6 py-4">
                 <div className="font-bold text-blue-500 text-xl mb-2">
@@ -20,7 +26,13 @@ const ImageCard = ({ image, searchTerm }) => {
             </div>
             <div className="px-6 py-4">
                 {tags.map((tag, index) => (
-                    <button onClick={() => searchTerm(tag)} type="button" key={index} className="inline-block bg-green-400 rounded-3xl px-3 py-1 text-sm font-semibold text-white m-2 duration-500 hover:scale-105 hover:shadow-xl">#{tag}</button>
+                    <button
+                        onClick={() => searchTerm(tag)}
+                        type="button" key={index}
+                        className="inline-block bg-green-400 rounded-3xl px-3 py-1 text-sm font-semibold text-white m-2 duration-500 hover:scale-105 hover:shadow-xl"
+                    >
+                        #{tag}
+                    </button>
                 ))}
             </div>
         </div >
